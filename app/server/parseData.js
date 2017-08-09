b3ece07ac1b54073450dc9c28c1f822c64c04869
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-08-09 14:20:12 -0400

    reworked data parser to use a more elegant promise based solution

--- app/server/parseData.js
+++ app/server/parseData.js
@@ -4,66 +4,81 @@ const db = require('./db');
 
 
 function expand(parents, foundActors, baconNumber) {
-	console.log('calling expand');
+	console.log('calling expand w/ number ', baconNumber);
 	if (baconNumber == 2) {
 		return;
 	}
 
 	let movies = new Set();
+	let costars;
+	
+	let movieReference = [];
+	let actorReference = [];
+	let actorTree = [];
 
 	tsv.getCostars(parents, foundActors, movies).then(children => {
 		console.log('got costars');
-		let actorTree = [];
+		costars = children;
 		parents.clear();
+		
+		return tsv.getMoviesByTconsts(movies);
+	
+	}).then(titles => {
+		console.log('got titles');
+	
+		movies = null;
+		
+		for (let [tconst, title] of titles.entries()) {
+			movieReference.push({
+				tconst: Number(tconst.slice(2)),
+				title
+			});
+		}
+		
+		for (let [child, [parent, tconst]] of costars.entries()) {
+			if (titles.has(tconst)) {
+				parents.add(child);
+				
+				actorTree.push({ 
+					nconst: Number(child.slice(2)),
+					parent: Number(parent.slice(2)),
+					tconst: Number(tconst.slice(2))
+				});
+			}
+		}
 
-		for (let [child, connection] of children.entries()) {
-			parents.add(child);
-			
-			actorTree.push({ 
-				nconst: Number(child.slice(2)),
-				parent: Number(connection[0].slice(2)),
-				tconst: Number(connection[1].slice(2))
+		costars = null;
+
+		return tsv.getActorsNames(parents);
+
+	}).then(names => {
+		console.log('got names');
+
+		for (let [nconst, name] of names.entries()) {
+			actorReference.push({
+				nconst: Number(nconst.slice(2)),
+				name,
+				number: baconNumber
 			});
 		}
 
-		Promise.all(tsv.getMoviesByTconsts(movies), tsv.getActorsNames(parents)).then(values => {
-			console.log('got titles and names');
-			movies = null;
-			let titles = [], actors = [];
+		return db.addTitleReferences(movieReference);
 
-			for (let [tconst, title] of values[0].entries()) {
-				titles.push({
-					tconst: Number(tconst.slice(2)),
-					title
-				});
-			}
+	}).then(() => {
+		console.log('sent titles');
+		movieReference = null;
+		return db.addActorReferences(actorReference);
 
-			for (let [nconst, name] of values[1].entries()) {
-				actors.push({
-					nconst: Number(nconst.slice(2)),
-					name,
-					number: baconNumber
-				});
-			}
+	}).then(() => {
+		console.log('sent actor names');
+		actorReference = null;
+		return db.addTreeTable(actorTree, baconNumber - 1);
 
-			return [titles, actors];
-		}).then(values => {
-			// while another Promise.all would be more elegant, we don't want these fighting over
-			// the same database
-			db.addTitleReferences(values[0]).then(res => {
-				console.log('sent titles');
-				db.addActorReferences(values[1]).then(res => {
-					console.log('sent actor names');
-					db.addTreeTable(actorTree, baconNumber - 1).then(res => {
-						console.log('sent actor tree');
-						values = null;
-						actorTree = null;
-						
-						expand(parents, foundActors, baconNumber + 1);
-					});
-				});
-			});
-		});
+	}).then(() => {
+		console.log('sent actor tree');
+		actorTree = null;
+		expand(parents, foundActors, baconNumber + 1);
+	
 	});
 }
 
