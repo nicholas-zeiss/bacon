be86f9947939634810dd241d1690d1a2d6340ea9
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-08-15 13:57:29 -0400

    cleaned and commented getData.js

--- app/server/getData.js
+++ app/server/getData.js
@@ -1,3 +1,8 @@
+/**
+ * This module downloads the IMDb dataset from AWS. Run this for title.basics.tsv.gz, title.principals.tsv.gz, and name.basics.tsv.
+ */
+
+
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const out = require('fs').createWriteStream('title.basics.tsv.gz', 'binary');
