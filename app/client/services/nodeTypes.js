b14fbe2feac30d2fccd98342b9c6332c6fb38bff
nicholas-zeiss <nicholas.zeiss@gmail.com>
2017-08-23 14:59:31 -0400

    wrote out all the node types for the bacon path for all devices and path lengths, adding it as a service

--- app/client/services/nodeTypes.js
+++ app/client/services/nodeTypes.js
@@ -13,17 +13,206 @@ import angular from 'angular';
 
 
 		function small(length) {
+			let types = [
+				3: [
+					'actor',
+					'right',
+					'actor'
+				],
+				5: [
+					'actor',
+					'right',
+					'actor',
+					'rightCenter',
+					'actor'
+				],
+				7: [
+					'actor',
+					'right',
+					'actor',
+					'downOnRight',
+					'actor',
+					'left',
+					'actor'
+				],
+				9: [
+					'actor',
+					'right',
+					'actor',
+					'downOnRight',
+					'actor',
+					'left',
+					'actor',
+					'leftCenter',
+					'actor'
+				],
+				11: [
+					'actor',
+					'right',
+					'actor',
+					'downOnRight',
+					'actor',
+					'left',
+					'actor',
+					'downOnLeft',
+					'actor',
+					'right',
+					'actor'
+				],
+				13: [
+					'actor',
+					'right',
+					'actor',
+					'downOnRight',
+					'actor',
+					'left',
+					'actor',
+					'downOnLeft',
+					'actor',
+					'right',
+					'actor',
+					'rightCenter',
+					'actor'
+				]
+			];
 
+			return types[length];
 		}
 
 
 		function medium(length) {
+			let types = [
+				3: [
+					'actor',
+					'right',
+					'actor'
+				],
+				5: [
+					'actor',
+					'rightShort',
+					'actor',
+					'rightShort',
+					'actor'
+				],
+				7: [
+					'actor',
+					'rightShort',
+					'actor',
+					'rightShort',
+					'actor'
+					'rightCenterLong',
+					'actor'
+				],
+				9: [
+					'actor',
+					'rightShort',
+					'actor',
+					'rightShort',
+					'actor'
+					'rightCenterShort',
+					'actor',
+					'leftShort',
+					'actor'
+				],
+				11: [
+					'actor',
+					'rightShort',
+					'actor',
+					'rightShort',
+					'actor'
+					'downOnRight',
+					'actor',
+					'leftShort',
+					'actor',
+					'leftShort',
+					'actor'
+				],
+				13: [
+					'actor',
+					'rightShort',
+					'actor',
+					'rightShort',
+					'actor'
+					'downOnRight',
+					'actor',
+					'leftShort',
+					'actor',
+					'leftShort',
+					'actor',
+					'leftCenterLong',
+					'actor'
+				]
+			];
 
+			return types[length];
 		}
 
 
 		function large(length) {
+let types = [
+				3: [
+					'actor',
+					'right',
+					'actor'
+				],
+				5: [
+					'actor',
+					'right',
+					'actor',
+					'right',
+					'actor'
+				],
+				7: [
+					'actor',
+					'curvedTop',
+					'actor',
+					'curvedTop',
+					'actor',
+					'curvedBottom',
+					'actor'
+				],
+				9: [
+					'actor',
+					'curvedTop',
+					'actor',
+					'curvedTop',
+					'actor',
+					'curvedBottom',
+					'actor'
+					'up',
+					'actor'
+				],
+				11: [
+					'actor',
+					'curvedTop',
+					'actor',
+					'curvedTop',
+					'actor',
+					'curvedBottom',
+					'actor'
+					'curvedBottom',
+					'actor'
+					'upRight',
+					'actor'
+				],
+				13: [
+					'actor',
+					'curvedTop',
+					'actor',
+					'curvedTop',
+					'actor',
+					'curvedTop',
+					'actor'
+					'curvedBottom',
+					'actor'
+					'curvedBottom',
+					'actor',
+					'upRight',
+					'actor'
+				]
+			];
 
+			return types[length];
 		}
 
 
